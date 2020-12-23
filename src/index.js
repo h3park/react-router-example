85001d4e3736704195c3d149a8525ec8e8b94e29
h3park <h3park@ucsd.edu>
2020-12-23 13:09:50 -0800

    import components on index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 
+import Home from './component/home';
+import Posts from './component/posts';
+import Profile from './component/profile';
+
 const App = () => (
   <div>
     Hello World

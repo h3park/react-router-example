286bf503897f14f9b35e57a354f6bc6e695dba39
h3park <h3park@ucsd.edu>
2020-12-23 13:58:32 -0800

    add Link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import Home from './component/home';
 import Posts from './component/posts';
@@ -8,6 +8,11 @@ import Profile from './component/profile';
 
 const App = () => (
   <BrowserRouter>
+    <header>
+      <Link to="/">Home</Link><br/>
+      <Link to="/posts">Posts</Link><br/>
+      <Link to="/profile">Profile</Link><br/>
+    </header>
     <Route path="/" exact component={Home}/>
     <Route path="/posts" component={Posts}/>
     <Route path="/profile"  component={Profile}/>

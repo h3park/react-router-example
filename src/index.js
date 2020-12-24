97a211ac2022e8d9dc208c63b097c5731aa3c222
h3park <h3park@ucsd.edu>
2020-12-23 21:34:06 -0800

    change link to navlink & add active style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, HashRouter, MemoryRouter, NavLink} from 'react-router-dom';
 
 import Home from './component/home';
 import Posts from './component/posts';
@@ -11,14 +11,18 @@ const App = () => (
   //pages are not reloading -> changing url dynamically and getting component we need 
   <BrowserRouter>
     <header>
-      <Link to="/">Home</Link><br/>
-      <Link to="/posts">Posts</Link><br/>
-      <Link to={{
+      <NavLink to="/" exact activeStyle={{color: 'green'}}>Home</NavLink><br/>
+
+      <NavLink to="/posts" exact activeStyle={{color: 'green'}}>Posts</NavLink><br/>
+      
+      <NavLink to={{
         pathname: '/profile',
         search: '?profile=true',
         hash: '#HR'
-      }}>Profile</Link><br/>
+      }} exact activeStyle={{color: 'green'}}>Profile</NavLink><br/>
+    
     </header>
+    
     <Route path="/" exact component={Home}/>
     <Route path="/posts" exact component={Posts}/>
     <Route path="/posts/:id" component={PostItem}/>
